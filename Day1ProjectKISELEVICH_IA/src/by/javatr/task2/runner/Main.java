2f3b61b82232dde3f43373634e5d245822dfb415
ivankiselevich <gref2259@gmail.com>
2019-12-10 20:02:46 +0300

    add Day1_task2 temp1

---
+++ Day1ProjectKISELEVICH_IA/src/by/javatr/task2/runner/Main.java
@@ -0,0 +1,16 @@
+package by.javatr.task2.runner;
+
+import by.javatr.scanner.CustomScanner;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input year");
+        int year = -1;
+        while(year >= 0 && year <= Date) {
+            year = CustomScanner.readInteger(System.in);
+        }
+
+    }
+}
