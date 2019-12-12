e07a24097fb282206740ec30a8aa98ddbc968513
ivankiselevich <gref2259@gmail.com>
2019-12-12 16:33:23 +0300

    add Day1_task1 tests

--- Day1ProjectKISELEVICH_IA/src/by/javatr/task1/unit/Unit.java
+++ Day1ProjectKISELEVICH_IA/src/by/javatr/task1/unit/Unit.java
@@ -2,12 +2,10 @@ package by.javatr.task1.unit;
 
 public class Unit {
 
-    //todo tests
     public static int getLastDigit(int number) {
         return ( Math.abs(number) % 10);
     }
-
-    //todo tests
+    
     public static int getLastDigitOfSqr(int number) {
         return ((number * number) % 10);
     }
---
+++ Day1ProjectKISELEVICH_IA/src/by/javatr/task1/unit/test/UnitTest.java
@@ -0,0 +1,93 @@
+package by.javatr.task1.unit.test;
+
+import by.javatr.task1.unit.Unit;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest extends Assert {
+/*
+    getLastDigit(int number) TESTS
+ */
+    @Test
+    public void getLastDigitTest1() {
+        int number = -1;
+        int expected = 1;
+        int result = Unit.getLastDigit(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitTest2() {
+        int number = 0;
+        int expected = 0;
+        int result = Unit.getLastDigit(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitTest3() {
+        int number = 1;
+        int expected = 1;
+        int result = Unit.getLastDigit(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitTest4() {
+        int number = 12;
+        int expected = 2;
+        int result = Unit.getLastDigit(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitTest5() {
+        int number = -34534;
+        int expected = 4;
+        int result = Unit.getLastDigit(number);
+        Assert.assertEquals(expected, result);
+    }
+
+/*
+    getLastDigitOfSqr(int number) TESTS
+ */
+    @Test
+    public void getLastDigitOfSqrTest1() {
+        int number = -1;
+        int expected = 1;
+        int result = Unit.getLastDigitOfSqr(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitOfSqrTest2() {
+        int number = 0;
+        int expected = 0;
+        int result = Unit.getLastDigitOfSqr(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitOfSqrTest3() {
+        int number = 1;
+        int expected = 1;
+        int result = Unit.getLastDigitOfSqr(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitOfSqrTest4() {
+        int number = 12;
+        int expected = 4;
+        int result = Unit.getLastDigitOfSqr(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void getLastDigitOfSqrTest5() {
+        int number = -34534;
+        int expected = 6;
+        int result = Unit.getLastDigitOfSqr(number);
+        Assert.assertEquals(expected, result);
+    }
+}
