15611bac5b6b47f158a83a46752e62fc064f5b18
ivankiselevich <gref2259@gmail.com>
2019-12-13 10:35:30 +0300

    add day1_task5 tests

--- Day1ProjectKISELEVICH_IA/src/by/javatr/task5/unit/Unit.java
+++ Day1ProjectKISELEVICH_IA/src/by/javatr/task5/unit/Unit.java
@@ -3,7 +3,7 @@ package by.javatr.task5.unit;
 import by.javatr.task5.unit.exception.InvalidPerfectNumberException;
 
 public class Unit {
-    //todo test
+
     public static boolean isNumberPerfect(int number) throws InvalidPerfectNumberException {
         if (number < 1) {
             throw new InvalidPerfectNumberException("Invalid number argument, must be number >= 1");
---
+++ Day1ProjectKISELEVICH_IA/src/by/javatr/task5/unit/test/UnitTest.java
@@ -0,0 +1,48 @@
+package by.javatr.task5.unit.test;
+
+import by.javatr.task5.unit.Unit;
+import by.javatr.task5.unit.exception.InvalidPerfectNumberException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+/*
+    boolean isNumberPerfect(int number) throws InvalidPerfectNumberException
+*/
+    @Test
+    public void isNumberPerfectTest1() throws InvalidPerfectNumberException {
+        int number = 1;
+        boolean expected = false;
+        boolean result = Unit.isNumberPerfect(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void isNumberPerfectTest2() throws InvalidPerfectNumberException {
+        int number = 2;
+        boolean expected = false;
+        boolean result = Unit.isNumberPerfect(number);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test(expected = InvalidPerfectNumberException.class)
+    public void isNumberPerfectTest3() throws InvalidPerfectNumberException {
+        int number = 0;
+        Unit.isNumberPerfect(number);
+    }
+
+    @Test(expected = InvalidPerfectNumberException.class)
+    public void isNumberPerfectTest4() throws InvalidPerfectNumberException {
+        int number = -1;
+        Unit.isNumberPerfect(number);
+    }
+
+    @Test
+    public void isNumberPerfectTest5() throws InvalidPerfectNumberException {
+        int number = 6;
+        boolean expected = true;
+        boolean result = Unit.isNumberPerfect(number);
+        Assert.assertEquals(expected, result);
+    }
+}
