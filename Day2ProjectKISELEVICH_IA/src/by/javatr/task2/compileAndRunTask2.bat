f6e4c638bc0dd6a3ca8f5e3dacddca28a7093dec
Ivan Kiselevich <gref2259@gmail.com>
2019-12-14 13:10:48 +0300

    add day2_task2

---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task2/compileAndRunTask2.bat
@@ -0,0 +1,3 @@
+echo "Compiling and running task2..."
+javac -d ../../../../bin/ ./main/Main.java
+java -cp ../../../../bin/ by.javatr.task2.main.Main %*
---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task2/main/Main.java
@@ -0,0 +1,17 @@
+package by.javatr.task2.main;
+
+/**
+2.  Создать приложение, которое отображает в окне консоли аргументы командной строки метода 
+main() в обратном порядке. 
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.print(args[i] + " ");
+        }
+    }
+}
