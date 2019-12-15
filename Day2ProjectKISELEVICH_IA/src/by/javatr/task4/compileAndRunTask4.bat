ea0e179b0b6d6fea136739772ef2366af6bdad49
Ivan Kiselevich <gref2259@gmail.com>
2019-12-15 10:47:13 +0300

    add day2_task4

---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task4/compileAndRunTask4.bat
@@ -0,0 +1,3 @@
+echo "Compiling and running task4..."
+javac -d ../../../../bin/ ./main/Main.java
+java -cp ../../../../bin/ by.javatr.task4.main.Main %*
---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task4/main/Main.java
@@ -0,0 +1,26 @@
+package by.javatr.task4.main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+4.  Создать  приложение  для  ввода  пароля  из  командной  строки  и  сравнения  его  со  строкой-
+образцом. 
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        String secretPassword = "qwerty1234";
+        String input;
+        if (args.length < 1) {
+            System.out.println("No args");
+            return;
+        }
+        input = args[0];
+        if (secretPassword.equals(input)) {
+            System.out.println("Correct password!");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
