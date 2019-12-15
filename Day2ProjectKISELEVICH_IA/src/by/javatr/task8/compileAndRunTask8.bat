d7d63635dcbe34793852b21a9baca4b705bc885e
Ivan Kiselevich <gref2259@gmail.com>
2019-12-15 22:18:27 +0300

    add day2_task8

---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task8/compileAndRunTask8.bat
@@ -0,0 +1,3 @@
+echo "Compiling and running task8..."
+javac -d ../../../../bin/ ./main/ConsoleApp.java
+java -cp ../../../../bin/ by.javatr.task8.main.ConsoleApp %*
---
+++ Day2ProjectKISELEVICH_IA/src/by/javatr/task8/main/ConsoleApp.java
@@ -0,0 +1,13 @@
+package by.javatr.task8.main;
+
+/**
+8.  Запустить приложение  из командной строки (В текстовом документе подробно описать 
+последовательность выполнений с скриншортами) 
+ */
+public class ConsoleApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, console!");
+        
+    }
+}
