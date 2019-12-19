bccb5adefeef8c4f92b8f52290501aa9c96d9534
Ivan Kiselevich <gref2259@gmail.com>
2019-12-19 19:23:51 +0300

    add equals, hashCode, toString

--- Day3ProjectKISELEVICH_IA/src/by/javatr/entity/Ball.java
+++ Day3ProjectKISELEVICH_IA/src/by/javatr/entity/Ball.java
@@ -1,5 +1,7 @@
 package by.javatr.entity;
 
+import java.util.Objects;
+
 public class Ball {
     private BallColor ballColor;
     private double weight;
@@ -25,5 +27,25 @@ public class Ball {
         this.weight = weight;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return Double.compare(ball.weight, weight) == 0 &&
+                ballColor == ball.ballColor;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballColor, weight);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + "{" +
+                "ballColor=" + ballColor +
+                ", weight=" + weight +
+                '}';
+    }
 }
