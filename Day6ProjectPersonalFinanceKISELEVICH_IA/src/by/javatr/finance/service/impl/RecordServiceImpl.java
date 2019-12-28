d2fae8d2709c05ef9babe285de74ebfe4d71cd37
ivankiselevich <gref2259@gmail.com>
2019-12-28 16:19:58 +0300

    fix RecordServiceImpl - add validation

--- Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/service/impl/RecordServiceImpl.java
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/service/impl/RecordServiceImpl.java
@@ -5,10 +5,7 @@ import by.javatr.finance.dao.exception.AbstractDAOException;
 import by.javatr.finance.dao.factory.DAOFactory;
 import by.javatr.finance.entity.Record;
 import by.javatr.finance.service.RecordService;
-import by.javatr.finance.service.exception.AbstractServiceException;
-import by.javatr.finance.service.exception.record.AddRecordServiceException;
-import by.javatr.finance.service.exception.record.GetAllRecordsServiceException;
-import by.javatr.finance.service.exception.record.RemoveRecordServiceException;
+import by.javatr.finance.service.exception.record.*;
 import by.javatr.finance.service.impl.Validation.RecordValidator;
 
 import java.util.List;
@@ -20,8 +17,9 @@ public class RecordServiceImpl implements RecordService {
     private RecordDAO recordDAO = DAOFactory.getInstance().getRecordDAO();
 
     @Override
-    public void addRecord(Record record) throws AbstractServiceException {
+    public void addRecord(Record record) throws AddRecordServiceException, IncorrectRecordServiceException, IncorrectDateServiceException, IncorrectCauseServiceException {
         try {
+            recordValidator.checkRecord(record);
             recordDAO.addRecord(record);
         } catch (AbstractDAOException e) {
             throw new AddRecordServiceException(e);
@@ -29,8 +27,9 @@ public class RecordServiceImpl implements RecordService {
     }
 
     @Override
-    public void removeRecord(Record record) throws AbstractServiceException {
+    public void removeRecord(Record record) throws RemoveRecordServiceException, IncorrectRecordServiceException, IncorrectDateServiceException, IncorrectCauseServiceException {
         try {
+            recordValidator.checkRecord(record);
             recordDAO.removeRecord(record);
         } catch (AbstractDAOException e) {
             throw new RemoveRecordServiceException(e);
@@ -38,7 +37,7 @@ public class RecordServiceImpl implements RecordService {
     }
 
     @Override
-    public List<Record> getAllRecords() throws AbstractServiceException {
+    public List<Record> getAllRecords() throws GetAllRecordsServiceException {
         try {
             return recordDAO.getAllRecords();
         } catch (AbstractDAOException e) {
