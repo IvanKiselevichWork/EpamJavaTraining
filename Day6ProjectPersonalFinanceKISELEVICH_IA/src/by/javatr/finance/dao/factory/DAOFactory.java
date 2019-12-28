e61bebf3a59a4f9bed9e77bf563141fe523c918a
ivankiselevich <gref2259@gmail.com>
2019-12-28 14:48:44 +0300

    add DAOFactory

---
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/dao/factory/DAOFactory.java
@@ -0,0 +1,28 @@
+package by.javatr.finance.dao.factory;
+
+import by.javatr.finance.dao.RecordDAO;
+import by.javatr.finance.dao.UserDAO;
+import by.javatr.finance.dao.fileImpl.FileRecordDAO;
+import by.javatr.finance.dao.fileImpl.FileUserDAO;
+
+public final class DAOFactory {
+    private final static DAOFactory instance = new DAOFactory();
+
+    private final UserDAO fileUserDao = new FileUserDAO();
+    private final RecordDAO fileRecordDAO = new FileRecordDAO();
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getInstance() {
+        return instance;
+    }
+
+    public UserDAO getUserDAO() {
+        return fileUserDao;
+    }
+
+    public RecordDAO getRecordDAO() {
+        return fileRecordDAO;
+    }
+}
