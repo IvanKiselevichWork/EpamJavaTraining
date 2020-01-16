9330dc7083dee2e5fbd359178b3c267b77dd0cc1
Ivan Kiselevich <gref2259@gmail.com>
2020-01-16 21:44:12 +0300

    add task3_1 regexp

--- Day7ProjectKISELEVICH_IA/src/by/javatr/task3/runner/Main.java
+++ Day7ProjectKISELEVICH_IA/src/by/javatr/task3/runner/Main.java
@@ -1,15 +1,17 @@
 package by.javatr.task3.runner;
 
-import by.javatr.task3.service.noregexp.TextService;
+import by.javatr.task3.service.regexp.TextService;
 
 public class Main {
     public static void main(String[] args) {
 
         TextService textService = new TextService();
 
-        String s1 = "123456789000";
+
+        String s1 = "qwe ert tyurty";
         System.out.println(textService.subTask1(s1, 3, 'X'));
 
+        /*
         String s2 = "РАБОТ РАЛИКИ РОЛЛ Р";
         System.out.println(textService.subTask2(s2));
 
@@ -21,5 +23,7 @@ public class Main {
 
         String s5 = " sdf sdfg . FFD dsfg йцу кен гшщ апр олд йцукен";
         System.out.println(textService.subTask5(s5, 3));
+
+         */
     }
 }
---
+++ Day7ProjectKISELEVICH_IA/src/by/javatr/task3/service/regexp/TextService.java
@@ -0,0 +1,36 @@
+package by.javatr.task3.service.regexp;
+
+import by.javatr.task3.service.exception.StringIsNullRuntimeException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextService {
+
+    /**
+     *  В каждом слове k-ю букву заменить заданным символом. Если k больше длины слова,
+     * корректировку не выполнять.
+     * @param s source string
+     * @param k step
+     * @param c char value will be set
+     * @return transformed string
+     * @throws StringIsNullRuntimeException if string is null
+     */
+    public String subTask1(String s, int k, char c) throws StringIsNullRuntimeException {
+        checkString(s);
+
+        String regexp = "([a-zA-Zа-яА-Я]{" + (k - 1) + "})([a-zA-Zа-яА-Я]{1})";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.replaceAll("$1" + c);
+    }
+
+    private void checkString(String s) throws StringIsNullRuntimeException {
+        if (s == null) {
+            throw new StringIsNullRuntimeException("string is null");
+        }
+    }
+
+}
