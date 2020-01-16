4fe7240e05cfd024b952007d25e3cdbb1136c968
Ivan Kiselevich <gref2259@gmail.com>
2020-01-16 18:31:11 +0300

    add task3_2 noregexp, add getStringBuilder method for lower memory use

--- Day7ProjectKISELEVICH_IA/src/by/javatr/task3/service/noregexp/TextService.java
+++ Day7ProjectKISELEVICH_IA/src/by/javatr/task3/service/noregexp/TextService.java
@@ -19,10 +19,30 @@ public class TextService {
         if (k >= s.length()) {
             return s;
         }
-        StringBuilder stringBuilder = new StringBuilder(s.length());
-        stringBuilder.append(s);
+        StringBuilder stringBuilder = getStringBuilder(s);
         for(int i = k; i < s.length(); i += k) {
             stringBuilder.setCharAt(i, c);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * В тексте после буквы Р, если она не последняя в слове, ошибочно напечатана буква А
+     * вместо О. Внести исправления в текст.
+     * @param s source string
+     * @return transformed string
+     * @throws StringIsNullRuntimeException if string is null
+     */
+    public String subTask2(String s) throws StringIsNullRuntimeException {
+        checkString(s);
+
+        StringBuilder stringBuilder = getStringBuilder(s);
+        for(int i = 0; i < s.length(); i++) {
+            if (i != s.length() - 1) {
+                if (s.charAt(i) == 'Р' && s.charAt(i + 1) == 'А') {
+                    stringBuilder.setCharAt(i + 1, 'О');
+                }
+            }
         }
         return stringBuilder.toString();
     }
@@ -31,6 +51,18 @@ public class TextService {
         if (s == null) {
             throw new StringIsNullRuntimeException("string is null");
         }
+    }
+
+    /**
+     * for lower memory use
+     * (if we use new StringBuilder(s) allocates s.length() + 16 elements)
+     * @param s
+     * @return
+     */
+    private StringBuilder getStringBuilder(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s.length());
+        stringBuilder.append(s);
+        return stringBuilder;
     }
 
 }
