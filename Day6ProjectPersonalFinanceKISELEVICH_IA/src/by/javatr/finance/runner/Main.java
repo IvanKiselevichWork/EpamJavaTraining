3460e819a1fe4b96cd9fe56795e86564b30c5e1c
ivankiselevich <gref2259@gmail.com>
2020-01-17 11:29:46 +0300

    fix View and runner

--- Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/runner/Main.java
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/runner/Main.java
@@ -1,15 +1,10 @@
 package by.javatr.finance.runner;
 
-import by.javatr.finance.controller.MainMenuController;
+import by.javatr.finance.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            MainMenuController.getInstance().execute(MainMenuController.RUN_MAIN_MENU_COMMAND, new CommandParameters());
-        } catch (Exception e) {
-            System.out.println("If you see this message, something went wrong!");
-            e.printStackTrace();
-        }
+        new View().runView();
     }
 }
--- Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/view/View.java
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/view/View.java
@@ -1,5 +1,7 @@
 package by.javatr.finance.view;
 
+import by.javatr.finance.controller.Controller;
+import by.javatr.finance.controller.exception.AbstractControllerException;
 import by.javatr.finance.entity.Record;
 
 import java.util.List;
@@ -7,17 +9,44 @@ import java.util.Scanner;
 
 public class View {
 
-    private static final View instance = new View();
+    private Controller controller;
 
-    public static View getInstance() {
-        return instance;
+    private final static String BASE_REQUEST = "SHOW_MAIN_MENU;";
+
+    public View() {
+        controller = new Controller();
     }
 
-    private View(){
+    public void runView() {
+        String request = BASE_REQUEST;
+        String response = null;
+        while (true) {
+            response = sendRequest(request);
+            request = handleResponse(response);
+        }
+    }
+
+    private String sendRequest(String request) {
+        String response = null;
+
+        try {
+            response = controller.execute(request);
+        } catch (AbstractControllerException e) {
+            // todo log
+            showErrorMessage("Controller broken!");
+            System.exit(-1);
+        }
+        return response;
 
     }
 
-    public void showMainMenu() {
+    private String handleResponse(String response) {
+        String request = null;
+
+        return request;
+    }
+
+    private void showMainMenu() {
         System.out.println("--------------------------");
         System.out.println("Main menu:");
         System.out.println("1 - Sign in");
@@ -26,7 +55,7 @@ public class View {
         System.out.println("--------------------------");
     }
 
-    public void showUserMenu() {
+    private void showUserMenu() {
         System.out.println("--------------------------");
         System.out.println("User menu:");
         System.out.println("1 - Show all my records");
@@ -36,7 +65,7 @@ public class View {
         System.out.println("--------------------------");
     }
 
-    public String getCommand(String message, String[] validCommands) {
+    private String getCommand(String message, String[] validCommands) {
         Scanner scanner = new Scanner(System.in);
         String command;
         MAIN_LOOP:
@@ -53,41 +82,41 @@ public class View {
         return command;
     }
 
-    public String getString(String message) {
+    private String getString(String message) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(message);
         return scanner.nextLine();
     }
 
-    public void showErrorMessage(String message) {
+    private void showErrorMessage(String message) {
         System.out.println("Error occurred: " + message);
     }
 
-    public void showExitMessage() {
+    private void showExitMessage() {
         System.out.println("Turning off...");
     }
 
-    public void welcomeUser(String login) {
+    private void welcomeUser(String login) {
         System.out.println("Welcome, " + login + "!");
     }
 
-    public void signInDataRequest() {
+    private void signInDataRequest() {
         System.out.println("Input data for signing in: ");
     }
 
-    public void registrationDataRequest() {
+    private void registrationDataRequest() {
         System.out.println("Input data for registration: ");
     }
 
-    public void showAccountNotFoundMessage() {
+    private void showAccountNotFoundMessage() {
         System.out.println("Account not found!");
     }
 
-    public void showLoginInUseMessage() {
+    private void showLoginInUseMessage() {
         System.out.println("Login in use!");
     }
 
-    public void showRecordList(List<Record> recordList) {
+    private void showRecordList(List<Record> recordList) {
         int index = 1;
         System.out.println("Records:");
         for (Record record : recordList) {
@@ -95,11 +124,11 @@ public class View {
         }
     }
 
-    public void showRecordAddedMessage(Record record) {
+    private void showRecordAddedMessage(Record record) {
         System.out.println("Record added: \n" + record);
     }
 
-    public void showRecordRemovedMessage() {
+    private void showRecordRemovedMessage() {
         System.out.println("Record removed");
     }
 }
