d4599a44bf457a098ff1f042a5fcd370c97b2b2a
Ivan Kiselevich <gref2259@gmail.com>
2020-01-18 18:18:14 +0300

    fix Main

--- Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/runner/Main.java
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/runner/Main.java
@@ -1,5 +1,6 @@
 package by.javatr.finance.runner;
 
+import by.javatr.finance.controller.CommandName;
 import by.javatr.finance.controller.CommandParameters;
 import by.javatr.finance.controller.Controller;
 import by.javatr.finance.controller.exception.AbstractControllerException;
@@ -8,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         CommandParameters commandParameters = new CommandParameters();
+        commandParameters.setParameter(CommandParameters.NEXT_COMMAND, CommandName.SHOW_MAIN_MENU);
         Controller controller = new Controller();
         try {
             while (true) {
