220db6a6d3c72caecfcc93418576678ae5651d41
ivankiselevich <gref2259@gmail.com>
2020-01-21 11:06:21 +0300

    create logger

---
+++ Day6ProjectPersonalFinanceKISELEVICH_IA/src/by/javatr/finance/logger/Logger.java
@@ -0,0 +1,81 @@
+package by.javatr.finance.logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+
+public class Logger {
+
+    enum LogLevel {
+        DEBUG,
+        WARNING,
+        ERROR
+    }
+
+    private Class<?> targetClass;
+    private LogWriter logWriter;
+
+    public static Logger getLogger(Class<?> targetClass) {
+        return new Logger(targetClass);
+    }
+
+    private Logger(Class<?> targetClass) {
+        this.targetClass = targetClass;
+        logWriter = LogWriter.getInstance();
+    }
+
+    public void debug(String message) {
+        StringBuffer stringBuffer = createMessage(message, LogLevel.DEBUG);
+        logWriter.write(stringBuffer.toString());
+    }
+
+    public void warning(String message) {
+        StringBuffer stringBuffer = createMessage(message, LogLevel.WARNING);
+        logWriter.write(stringBuffer.toString());
+    }
+
+    public void error(String message) {
+        StringBuffer stringBuffer = createMessage(message, LogLevel.ERROR);
+        logWriter.write(stringBuffer.toString());
+    }
+
+    private StringBuffer createMessage(String message, LogLevel logLevel) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(logLevel.toString());
+        stringBuffer.append(" ; ");
+        stringBuffer.append(LocalDateTime.now());
+        stringBuffer.append(" ; ");
+        stringBuffer.append("Class name: ");
+        stringBuffer.append(targetClass.getName());
+        stringBuffer.append(" ; ");
+        stringBuffer.append(message);
+        stringBuffer.append(" ; \n");
+        return stringBuffer;
+    }
+
+    private static class LogWriter {
+        private static class LogWriterHolder {
+            private static LogWriter instance = new LogWriter();
+        }
+
+        public static LogWriter getInstance() {
+            return LogWriterHolder.instance;
+        }
+
+        private static final String LOG_FILEPATH = "log.txt";
+
+        private LogWriter() {
+
+        }
+
+        public void write(String message) {
+            try {
+                Files.write(Paths.get(LOG_FILEPATH), message.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException ignored) {
+
+            }
+        }
+    }
+}
